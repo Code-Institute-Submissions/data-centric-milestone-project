b4d38dee149d3a5e2ed376f7533bd4e622362e87
Joseph Comerford <joseph_comerford@msn.com>
2019-03-17 16:24:10 +0000

    connected to DB and added All Characters page

--- app.py
+++ app.py
@@ -1,12 +1,56 @@
+import pymongo
 import os
-from flask import Flask
+from flask import Flask, redirect, request, url_for, render_template
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+
+"""
+Connects to database
+"""
+
+app.config["MONGODB_NAME"] = 'game_of_thrones_characters'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
+
+#Prints all characters to Python
+
+# def mongo_connect(url):
+#     try:
+#         conn = pymongo.MongoClient(url)
+#         print('Mongo is connected')
+#         return conn
+#     except pymongo.errors.ConnectionFailure as e:
+#         print('Could not connect to MongoDB: %s') % e
+        
+# conn = mongo_connect(MONGODB_URI)
+
+# coll = conn[DBS_NAME][COLLECTION_NAME]
+
+# documents = coll.find()
+
+# for doc in documents:
+#     print(doc)
+
+
+"""
+Returns all characters to Flask app
+"""
+
 @app.route('/')
+@app.route('/get_all_characters')
 def hello():
-    return 'Hello Game of Thrones'
+    return render_template('all_characters.html', character=mongo.db.character.find())
     
+    
+
+    
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
